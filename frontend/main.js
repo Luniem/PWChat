57de182648540470114b8637a365d2bcca1415f5
Nico Knünz <nico.knuenz@aon.at>
2024-10-19 23:55:12 +0200

    feat: make menu-items be able to toggle

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,20 @@
 
 const nav = document.getElementsByTagName("nav")[0];
 const chatButton = document.getElementById("chat-button");
+const userButton = document.getElementById("user-button");
 
-chatButton.addEventListener('click', () => {
-    nav.classList.toggle("reduced")
-})
+chatButton.addEventListener('click', () => activateButton(chatButton));
+userButton.addEventListener('click', () => activateButton(userButton));
+
+function activateButton(button) {
+    const activeButton = getCurrentActiveNavItem();
+    activeButton.classList.toggle("active");
+    button.classList.toggle("active")
+}
+
+function getCurrentActiveNavItem() {
+    for(let i = 0; i < nav.children.length; i++) {
+        const currItem = nav.children.item(i);
+        if (currItem.classList.contains("active")) return currItem;
+    }
+}
